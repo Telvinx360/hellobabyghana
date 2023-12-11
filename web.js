67dca0673fb0dde8698ddea77183c6ac271a18b4
Telvinx360 <153513450+Telvinx360@users.noreply.github.com>
2023-12-11 13:10:20 +0100

    Add files via upload

---
+++ web.js
@@ -0,0 +1,13 @@
+function openlist(){
+    document.getElementById("list").style.display="block";
+    document.getElementById("backlist").style.display="block"
+    
+}
+function closelist(){
+    document.getElementById("list").style.display="none";
+    document.getElementById("backlist").style.display="none"
+}
+if(document.getElementById("list")){
+    document.getElementById("list").style.display="none";
+    document.getElementById("backlist").style.display="none"
+}
